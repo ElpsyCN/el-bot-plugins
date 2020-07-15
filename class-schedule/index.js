884ce9ef3768d26f4e3a72547664033c26396c3f
ADD-SP <me@addesp.com>
2020-07-15 15:18:01 +0800

    :triangular_flag_on_post: Expanded the syntax of keyword advance.

--- class-schedule/index.js
+++ class-schedule/index.js
@@ -7,6 +7,7 @@ import { sendMessageByConfig } from "@utils/message";
 const g_formatStr =
     "课程名：%s\n上课时间：%s:%s\n地点：%s\n授课老师：%s\n其它信息：%s";
 const g_timeRegexp = new RegExp("^(MON|TUE|WED|THU|FRI|SAT|SUB)\\s([0-9]{1,2}):([0-9]{1,2})$", "i");
+const g_advanceRegexp = new RegExp("^([0-9]+d)?([0-9]+h)?([0-9]+m)?$", "i");
 
 const g_weekMap = new Map();
 g_weekMap["SUN"] = 7; g_weekMap[7] = "SUN";
@@ -43,7 +44,7 @@ function procCourses(courseGroup) {
 
     for (let course of courseGroup.courses) {
         // 在不写 advance 的情况下默认提前十分钟提醒
-        let time = parseTime(course, courseGroup.advance ? courseGroup.advance : "10m");
+        let time = parseTimeAndAdvance(course, courseGroup.advance ? courseGroup.advance : "10m");
         let place = course.place ? course.place.toString() : "未提供该信息";
         let teacher = course.teacher ? course.teacher.toString() : "未提供该信息";
         let other = course.other ? course.other.toString() : "无";
@@ -65,7 +66,17 @@ function procCourses(courseGroup) {
     }
 }
 
-function parseTime(course, advance) {
+function parseAdvance(time, advance) {
+    let matches = g_advanceRegexp.exec(advance);
+    if (matches == null) {
+        invalidError(advance);
+    }
+    time.advance.day = matches[1];
+    time.advance.hour = matches[2];
+    time.advance.minute = matches[3];
+}
+
+function parseTimeAndAdvance(course, advance) {
     let time = {
         // 原始字符串
         native: undefined,
@@ -78,12 +89,14 @@ function parseTime(course, advance) {
         // 经过处理后生成的 cron 字符串，用于启动定时任务。
         cron: undefined,
         advance: {
+            // 提醒时间-提前多少天
+            day: undefined,
+            // 提醒时间-星期
+            week: undefined,
             // 提醒时间-小时
             hour: undefined,
             // 提醒时间-分钟
             minute: undefined,
-            // 提醒时间-星期
-            week: undefined,
         },
     };
     // 「课程开始时间」和「课程名」必须在配置文件中提供
@@ -96,15 +109,13 @@ function parseTime(course, advance) {
     // 分离出课程开始时间的 hour、minute 和 week
     let matches = g_timeRegexp.exec(time.native);
     if (matches == null || matches.length != 4) {
-        let errStr = util.format("[%s] is invalid.", time.native);
-        throw new ConfigSyntaxError(errStr);
+        nvalidError(time.native);
     }
     time.week = matches[1].toUpperCase();
     time.hour = matches[2];
     time.minute = matches[3];
     if (parseInt(time.week) >= 24 || parseInt(time.minute) >= 60) {
-        let errStr = util.format("[%s] is invalid.", time.native);
-        throw new ConfigSyntaxError(errStr);
+        invalidError(time.advance);
     }
 
 
@@ -112,13 +123,17 @@ function parseTime(course, advance) {
     let temp = dayjs().set("hour", parseInt(time.hour))
         .set("minute", parseInt(time.minute))
         .set("day", g_weekMap[time.week]);
-    advance = course.advance ? course.advance : advance.toString();
+    parseAdvance(time, course.advance ? course.advance.toString() : advance.toString())
     temp.day(g_weekMap[time.week]);
     // 使用「课程开始时间」减去「提前时长」得到「提醒时间」
-    temp = temp.subtract(
-        parseInt(advance.substr(0, advance.length - 1)),
-        advance[advance.length - 1]
-    );
+    for (let advance of [time.advance.day, time.advance.hour, time.advance.minute]) {
+        if (!advance) continue;
+        temp = temp.subtract(
+            parseInt(advance.substr(0, advance.length - 1)),
+            advance[advance.length - 1]
+        ); ``
+    }
+
     time.advance.week = g_weekMap[temp.day()];
     time.advance.hour = temp.get("h");
     time.advance.minute = temp.get("m");
@@ -134,6 +149,10 @@ function parseTime(course, advance) {
     return time;
 }
 
+function invalidError(str) {
+    let errStr = util.format("[%s] is invalid.", str);
+    throw new ConfigSyntaxError(errStr);
+}
 
 class ConfigSyntaxError extends Error {
     constructor(message) {
